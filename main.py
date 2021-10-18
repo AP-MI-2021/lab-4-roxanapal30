99fc1781dfd0679c66a8e1150c1b9ca4f268b17f
Roxana Paladi <roxana300302@gmail.com>
2021-10-18 20:01:17 +0300

    prob

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+'''
+functia citire citeste lista data
+'''
 def citirelista():
     l=[]
     string=input("Dati lista cu elemente separate cu virgula:")
@@ -6,20 +9,30 @@ def citirelista():
         l.append(int(x))
     return l
 
+'''
+functia lista_fara_nr_prime elimina numerele prime din listă
+'''
 def lista_fara_nr_prime(l):
     rezultat=[]
     for i in range (len(l)):
-        ok=1
-        if l[i]<=1:
-            return false
+        y=l[i]
+        nrprim=True
+        if y==1:
+            rezultat.append(y)
+        elif y==0:
+            rezultat.append(y)
         else:
-            for d in range (2,l[i]//2):
-                if l[i]%d==0:
-                    ok=0
-            if ok==1:
-                rezultat.append(l[i])
+            for d in range (2,y//2+1):
+                if y%d==0:
+                    nrprim=False
+            if nrprim==False:
+                rezultat.append(y)
     return rezultat
 
+
+'''
+functia numar_mai_mare_decat_media_nr dacă media aritmetică a numerelor este mai mare decât un număr x dat
+'''
 def numar_mai_mare_decat_media_nr(l,x):
     suma=0
     for i in range (len(l)):
@@ -29,34 +42,50 @@ def numar_mai_mare_decat_media_nr(l,x):
     else:
         return False
 
+'''
+functia adaugare_nr_div_proprii adăuga după fiecare element numărul de divizori proprii ai elementului
+'''
 def adaugare_nr_div_proprii(l):
     rezultat=[]
     for i in range (len(l)):
         nrdediv=0
-        for d in range (2,l[i]//2):
+        for d in range (2,l[i]//2+1):
             if l[i]%d==0:
                 nrdediv=nrdediv+1
         rezultat.append(l[i])
         rezultat.append(nrdediv)
     return rezultat
 
-def test_lista_fara_nr_prime(l):
-    assert lista_fara_nr_prime(l)
-    assert lista_fara_nr_prime(l)
-    assert lista_fara_nr_prime(l)
+def lista_intiala_schimbata_cu_tuplu(l):
+    rez=l[:]
+    for i in range (len(l)):
+        rez[i][0]=l[i]
+        rez[i][1]=i
+        nr=0
+        for d in range (len(l)):
+            if l[i]==l[d]:
+                nr=nr+1
+        rez[i][2]=nr
+    return rez
 
-def test_numar_mai_mare_decat_media_nr(l,x):
-    assert numar_mai_mare_decat_media_nr(l,x)
-    assert numar_mai_mare_decat_media_nr(l, x)
-    assert numar_mai_mare_decat_media_nr(l, x)
 
-def test_adaugare_nr_div_proprii(l):
-    assert adaugare_nr_div_proprii(l)
-    assert adaugare_nr_div_proprii(l)
-    assert adaugare_nr_div_proprii(l)
+def test_lista_fara_nr_prime():
+    assert lista_fara_nr_prime([1,2,3,4])==[1,4]
+    assert lista_fara_nr_prime([1,7,9,20])==[1,9,20]
+
+def test_numar_mai_mare_decat_media_nr():
+    assert numar_mai_mare_decat_media_nr([1,2,3,4],1)==True
+    assert numar_mai_mare_decat_media_nr([1,2,3,4],3)==False
+
+def test_adaugare_nr_div_proprii():
+    assert adaugare_nr_div_proprii([9,7,20])==[9,1,7,0,20,4]
+    assert adaugare_nr_div_proprii([10,5,3])==[10,2,5,0,3,0]
 
 
 def main():
+    test_lista_fara_nr_prime()
+    test_numar_mai_mare_decat_media_nr()
+    test_adaugare_nr_div_proprii()
     while True:
         print("1.Citirea unei liste de numere întregi. Citirile repetate suprascriu listele precedente.")
         print("2.Afișarea listei după eliminarea numerelor prime din listă")
@@ -79,11 +108,10 @@ def main():
         elif optiune==4:
             print(adaugare_nr_div_proprii(l))
         elif optiune == 5:
-            print(2)
-
+            print(lista_intiala_schimbata_cu_tuplu(l))
         elif optiune == 6:
             break
 
 
 
-main()
+main(
